9a55be9e4dd46ad018049c2bd664c1a21e87f7b0
Evheniia <ev.monastyrna@outlook.com>
2020-07-27 09:45:12 +0300

    [CHG] remove useless configs, register a module with routes

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,15 @@ db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
-
-    db.init_app(app)
+    app = Flask(
+        __name__,
+        template_folder="templates")
     app.config.from_object('config.DevelopmentConfig')
+    db.init_app(app)
 
     with app.app_context():
-        from app.payment import controllers  # Import routes
+        from app.payment.controllers import module  # Import routes
         db.create_all()  # Create sql tables for our data models
-
+        app.url_map.strict_slashes = False
+        app.register_blueprint(module)
         return app
