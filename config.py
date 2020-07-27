c465540d18a819e26ed03894ee3e08bd175bf251
Evheniia <ev.monastyrna@outlook.com>
2020-07-27 09:45:50 +0300

    [CHN] update configs

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ class Config:
     DEBUG = False
     CSRF_ENABLED = True
     SECRET_KEY = '64LedGRHlXyHopEbcVMxj11pbSBosJPg'
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV')
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
